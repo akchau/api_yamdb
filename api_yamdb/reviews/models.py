f0e8c93aacbdf1f451b4b41d2929e096a4910275
netzen86 <netzen0su@gmail.com>
2022-06-14 23:49:31 +0500

    Создал каркас приложения

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Users(models.Model):
+    pass
+
+
+class Titles(models.Moldel):
+    pass
+
+
+class Categories(models.Model):
+    pass
+
+
+class Genres(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
